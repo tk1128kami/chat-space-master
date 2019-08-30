3de5d321c096fafe4f568aee2036c2ab7b4b4622
tk1128kami <tk1128kami@icloud.com>
2019-08-30 19:17:14 +0900

    Update users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -8,7 +8,7 @@ $(document).on('turbolink:load',function(){
             <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
           </div>`
           search_list.append(html)
-        }
+        };
     function appendErrMsgToHTML(msg){
       var html= `<p class="chat-group-user__name">${msg}</p>`
       search_list.append(html)
